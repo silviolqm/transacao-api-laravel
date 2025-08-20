786084ced146bdcfde664cc545745f9fc2661d49
silviolqm <silviolqm@gmail.com>
2025-08-20 13:45:09 -0300

    Correções de validation no controller

--- src/transacao-api/app/Http/Controllers/TransacaoController.php
+++ src/transacao-api/app/Http/Controllers/TransacaoController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\TransacaoResource;
 use App\Models\Transacao;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Storage;
 
 class TransacaoController extends Controller
 {
@@ -17,8 +18,8 @@ class TransacaoController extends Controller
     public function store(Request $request)
     {
         $data = $request->validate([
-            'valor' => ['required', 'numeric'],
-            'cpf' => ['required'],
+            'valor' => ['required', 'numeric', 'min:0.01'],
+            'cpf' => ['required', 'string', 'max:14'],
             'documento' => ['nullable', 'file', 'mimes:pdf,jpg,png', 'max:2048'],
             'status' => ['required', Rule::in(['Em processamento', 'Aprovada', 'Negada'])],
         ]);
@@ -43,11 +44,21 @@ class TransacaoController extends Controller
     public function update(Request $request, Transacao $transacao)
     {
         $data = $request->validate([
-            'valor' => ['required', 'numeric'],
-            'cpf' => ['required'],
-            'documento_path' => ['nullable'],
-            'status' => ['required', Rule::in(['Em processamento', 'Aprovada', 'Negada'])],
+            'valor' => ['sometimes','required', 'numeric', 'min:0.01'],
+            'cpf' => ['sometimes', 'required', 'string', 'max:14'],
+            'documento_path' => ['nullable', 'file', 'mimes:pdf,jpg,png', 'max:2048'],
+            'status' => ['sometimes', 'required', Rule::in(['Em processamento', 'Aprovada', 'Negada'])],
         ]);
+
+        if ($request->hasFile('documento')) {
+            //Apaga o arquivo antigo
+            if ($transacao->documento_path) {
+                Storage::disk('public')->delete($transacao->documento_path);
+            }
+            //Salva o novo arquivo
+            $path = $request->file('documento')->store('documentos', 'public');
+            $data['documento_path'] = $path;
+        }
 
         $transacao->update($data);
 
@@ -58,6 +69,6 @@ class TransacaoController extends Controller
     {
         $transacao->delete();
 
-        return response()->json();
+        return response()->noContent();
     }
 }
